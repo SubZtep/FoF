3f240871cbfd73927eece7c0e28bb575fc040cc3
Andras Serfozo <subztep@gmail.com>
2020-09-07 02:45:39 +0100

    :monkey_face:

--- src/components/audio/sound.ts
+++
@@ -1,9 +0,0 @@
-import { getAudioBuffer } from "../../audio/audio"
-
-Object.defineProperty(AFRAME.components.sound.Component.prototype, "audioLoader", {
-  value: {
-    load: async (src: string, onLoad: (audioBuffer: AudioBuffer) => void) => {
-      onLoad(await getAudioBuffer())
-    },
-  },
-})
--- src/components/environment/tree-simple.ts
+++
@@ -1,9 +0,0 @@
-// https://blog.mozvr.com/procedural-geometry-low-poly-clouds/
-import { geo, mat } from "../../3d/tree-oak"
-
-AFRAME.registerComponent("tree-simple", {
-  init() {
-    let tree = new THREE.Mesh(geo(), mat())
-    this.el.setObject3D("mesh", tree)
-  },
-})
